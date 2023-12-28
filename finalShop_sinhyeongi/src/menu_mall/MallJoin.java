bf5242f4a73aaf25ab624da774c022cfbc65b29a
신현기 <sinhg1221@daum.net>
2023-12-28 14:58:49 +0900

    Fix : 회원가입
    중복 아이디 입력 시 메인으로 이동 하도록 수정

--- finalShop_sinhyeongi/src/menu_mall/MallJoin.java
+++ finalShop_sinhyeongi/src/menu_mall/MallJoin.java
@@ -18,19 +18,18 @@ public class MallJoin implements MenuCommand {
 		System.out.println("=====[ 회원가입 ]=====");
 		MemberDAO dao = MemberDAO.getInstance();
 		String id = Util.getValue("아이디 ");
-		
-		if(dao.getMemberById(id)!= null) {
+
+		if (dao.getMemberById(id) != null) {
 			System.out.println("이미 사용하는 아이디");
-			return false;
-		}
-		
-		String pw = Util.getValue("비밀번호 ");
-		String name = Util.getValue("이름 ");
-		
-		if(dao.insertMember(id , pw , name)) {
-			System.out.println("[ 회원 추가 완료 ]");
-		}else {
-			System.out.println("[ 회원 추가 실패]");
+		} else {
+			String pw = Util.getValue("비밀번호 ");
+			String name = Util.getValue("이름 ");
+
+			if (dao.insertMember(id, pw, name)) {
+				System.out.println("[ 회원 추가 완료 ]");
+			} else {
+				System.out.println("[ 회원 추가 실패]");
+			}
 		}
 		cont.setNext("MallMain");
 		return false;
